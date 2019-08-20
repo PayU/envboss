56be693e8c33d33ba9deab020a879f0e65955711
yuri <yuri.abaev@zooz.com>
2019-08-20 16:41:27 +0300

    added types

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare module 'env-vars-config'
+interface ParamsConfiguration{
+    [name:string]:ParamConfiguration
+}
+interface ParamConfiguration{
+    mandatory?:boolean;
+    default?: PrimitiveType;
+    validValues?:PrimitiveType[]
+    wrappingFunction?:(value:any)=>any
+}
+
+type PrimitiveType = number | string | boolean
+export declare function createEnvObject(paramsConfig:ParamsConfiguration, shouldValidateEnvParams?:boolean): void;
+
