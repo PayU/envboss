c1dd2e1650d8262f7efde9f7db9c4ccf5add7680
yuri <yuri.abaev@zooz.com>
2020-06-24 13:54:08 +0300

    fix MR comments

--- src/index.js
+++ src/index.js
@@ -1,113 +1,109 @@
-const isEnvParamEmpty = ([paramName]) => !process.env[paramName] || process.env[paramName].trim() === ''
-const isMandatory = ([, envParamConfig]) => envParamConfig.mandatory
+const isEnvParamEmpty = ([paramName]) => !process.env[paramName] || process.env[paramName].trim() === '';
+const isMandatory = ([_, envParamConfig]) => envParamConfig.mandatory;
 
-function validateMandatoryEnvParams (paramsConfig) {
-    const missingFields = Object.entries(paramsConfig)
-        .filter(isMandatory)
-        .filter(isEnvParamEmpty)
+function validateMandatoryEnvParams(paramsConfig) {
+  const missingFields = Object.entries(paramsConfig)
+    .filter(isMandatory)
+    .filter(isEnvParamEmpty);
 
-    if (missingFields.length > 0) {
-        throw new Error(`missing mandatory env variables: ${JSON.stringify(missingFields.map(([paramName]) => paramName))}`)
-    }
+  if (missingFields.length > 0) {
+    throw new Error(`missing mandatory env variables: ${JSON.stringify(missingFields.map(([paramName]) => paramName))}`);
+  }
 }
 
-function applyWrappingFunction (paramName, wrappingFunction, value) {
-    if (typeof wrappingFunction !== 'function') {
-        throw new Error(`wrappingFunction method of '${paramName}' should be a function`)
-    }
-    return wrappingFunction(value)
+function applyWrappingFunction(paramName, wrappingFunction, value) {
+  if (typeof wrappingFunction !== 'function') {
+    throw new Error(`wrappingFunction method of '${paramName}' should be a function`);
+  }
+  return wrappingFunction(value);
 }
 
-function applyValidationFunction (paramName, config, value) {
-    if (typeof config.validationFunction !== 'function') {
-        throw new Error(`validationFunction method of '${paramName}' should be a function`)
-    }
-    if (!config.validationFunction(value)) {
-        throw new Error(`value '${value}' of env param '${paramName}' is not valid, check paramsConfig to see valid values`)
-    }
+function applyValidationFunction(paramName, config, value) {
+  if (typeof config.validationFunction !== 'function') {
+    throw new Error(`validationFunction method of '${paramName}' should be a function`);
+  }
+  if (!config.validationFunction(value)) {
+    throw new Error(`value '${value}' of env param '${paramName}' is not valid, check paramsConfig to see valid values`);
+  }
 }
 
-function getWrappingFunctionByDefaultValue (value) {
-    const wrappingFunction = getConvertingFunctionByType(typeof value)
-    return wrappingFunction
+function getWrappingFunctionByDefaultValue(value) {
+  const wrappingFunction = getConvertingFunctionByType(typeof value);
+  return wrappingFunction;
 }
 
-function getConvertingFunctionByType (type) {
-    let convertingFunc
-    switch (type) {
-        case Types.Number:
-            convertingFunc = Number
-            break
-        case Types.Boolean:
-            convertingFunc = (v) => v === 'true'
-            break
-        case Types.String:
-            convertingFunc = String
-            break
-        case Types.Array:
-            convertingFunc = (v) => v.split(',')
-            break
-        default:
-            convertingFunc = (v) => v
-    }
+function getConvertingFunctionByType(type) {
+  let convertingFunc;
+  switch (type) {
+    case Types.Number:
+      convertingFunc = Number;
+      break;
+    case Types.Boolean:
+      convertingFunc = (v) => v === 'true';
+      break;
+    case Types.String:
+      convertingFunc = String;
+      break;
+    case Types.Array:
+      convertingFunc = (v) => v.split(',');
+      break;
+    default:
+      convertingFunc = (v) => v;
+  }
 
-    return convertingFunc
-
+  return convertingFunc;
 }
 
-function convertValueToRequiredType (type, value) {
-    const convertingFunction = getConvertingFunctionByType(type)
-    return convertingFunction(value)
+function convertValueToRequiredType(type, value) {
+  const convertingFunction = getConvertingFunctionByType(type);
+  return convertingFunction(value);
 }
 
-function createEnvObject (paramsConfig, shouldValidateEnvParams = true) {
-    if (shouldValidateEnvParams) {
-        validateMandatoryEnvParams(paramsConfig)
-    }
-    const result = {}
-    Object.entries(paramsConfig)
-        .forEach(([paramName, config]) => {
-            let value = process.env[paramName] || config.default
+function createEnvObject(paramsConfig, shouldValidateEnvParams = true) {
+  if (shouldValidateEnvParams) {
+    validateMandatoryEnvParams(paramsConfig);
+  }
+  const result = {};
+  Object.entries(paramsConfig)
+    .forEach(([paramName, config]) => {
+      let value = process.env[paramName] || config.default;
 
-            if (config.wrappingFunction || config.type) {
-                if (config.type) {
-                    value = convertValueToRequiredType(config.type, value)
-                } else {
-                    value = applyWrappingFunction(paramName, config.wrappingFunction, value)
-                }
+      if (config.type) {
+        value = convertValueToRequiredType(config.type, value);
+      } else if (config.wrappingFunction) {
+        value = applyWrappingFunction(paramName, config.wrappingFunction, value);
+      } else {
+        const wrappingFunction = getWrappingFunctionByDefaultValue(config.default);
+        value = applyWrappingFunction(paramName, wrappingFunction, value);
+      }
 
-            } else {
-                const wrappingFunction = getWrappingFunctionByDefaultValue(config.default)
-                value = applyWrappingFunction(paramName, wrappingFunction, value)
-            }
+      if (shouldValidateEnvParams) {
+        if (config.validationFunction) {
+          applyValidationFunction(paramName, config, value);
+        }
 
-            if (shouldValidateEnvParams) {
-                if (config.validationFunction) {
-                    applyValidationFunction(paramName, config, value)
-                }
+        if (config.validValues) {
+          if (!config.validValues.includes(value)) {
+            throw new Error(`value '${value}' of env param '${paramName}' is not valid, check paramsConfig to see valid values`);
+          }
+        }
+      }
 
-                if (config.validValues) {
-                    if (!config.validValues.includes(value)) {
-                        throw new Error(`value '${value}' of env param '${paramName}' is not valid, check paramsConfig to see valid values`)
-                    }
-                }
-            }
+      result[paramName] = value;
+    });
 
-            result[paramName] = value
-        })
-
-    return result
+  return result;
 }
 
 const Types = {
-    Number: 'number',
-    Boolean: 'boolean',
-    String: 'string',
-    Array: 'Array'
-}
+  Number: 'number',
+  Boolean: 'boolean',
+  String: 'string',
+  Array: 'Array',
+};
 
 module.exports = {
-    createEnvObject,
-    mandatory: true,
-    Types
-}
+  createEnvObject,
+  mandatory: true,
+  Types,
+};
